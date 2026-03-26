2d791ae0543ac86f8ee230153d9024538207d2ab
Umidjon992 <umidjontorayev59@gmail.com>
2026-03-26 20:38:50 +0500

    Start

---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "Ali" },
+  { id: 2, name: "Vali" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUsers = {
+    id: Date.now(),
+    name: req.body.name,
+  };
+
+  users.push(newUsers);
+
+  res.status(201).json({
+    massage: "User created",
+    data: newUsers,
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id == req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  user.name = req.body.name;
+
+  res.json({
+    message: "User updated",
+    data: user,
+  });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const index = users.findIndex((u) => u.id == req.params.id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  users.splice(index, 1);
+
+  res.json({ message: "User deleted" });
+});
+
+app.listen(3000, () => {
+  console.log(`Server started on http://localhost:3000`);
+});
